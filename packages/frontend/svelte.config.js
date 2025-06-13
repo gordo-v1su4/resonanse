24915b3d7af18450dbc389be19b2efc2b24a26fe
gordo-v1su4 <gordo@v1su4.com>
2025-06-13 00:16:44 -0400

    fix: resolve merge conflicts in svelte.config.js

--- packages/frontend/svelte.config.js
+++ packages/frontend/svelte.config.js
@@ -1,20 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-<<<<<<< HEAD
-import { sequence } from '@sveltejs/kit/hooks';
-import { melt } from '@melt-ui/pp';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: sequence([vitePreprocess(), melt()]),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-=======
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -26,13 +11,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
->>>>>>> main
 		adapter: adapter()
 	}
 };
 
-<<<<<<< HEAD
 export default config;
-=======
-export default config;
->>>>>>> main
